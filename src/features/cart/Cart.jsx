9dfc44f4247f09519208665de8f10ef55dfc41ac
rohitumadi <rohitumadi2000@gmail.com>
2024-01-14 12:21:47 +0530

    tailwind css added

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,4 +1,7 @@
 import { Link } from 'react-router-dom';
+import LinkButton from '../../ui/LinkButton';
+import Button from '../../ui/Button';
+import CartItem from './CartItem';
 
 const fakeCart = [
   {
@@ -28,19 +31,21 @@ function Cart() {
   const cart = fakeCart;
 
   return (
-    <div>
-      <Link
-        to="/menu"
-        className="text-sm  text-blue-500 hover:text-blue-600 hover:underline"
-      >
-        &larr; Back to menu
-      </Link>
+    <div className="px-4 py-3">
+      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2>Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
+      <ul className="divide-y-2 divide-stone-200 border-b">
+        {cart.map((pizza) => (
+          <CartItem item={pizza} key={pizza.pizzaId} />
+        ))}
+      </ul>
+      <div className="mt-6 space-x-2">
+        <Button type="primary" to="/order/new">
+          Order pizzas
+        </Button>
 
-      <div>
-        <Link to="/order/new">Order pizzas</Link>
-        <button>Clear cart</button>
+        <Button type="secondary">Clear cart</Button>
       </div>
     </div>
   );
